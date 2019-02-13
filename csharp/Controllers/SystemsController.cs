2538e757c43f38577be0de3d69d9a8d01d1f0192
Dan Lawrence <daniellawrence2468@gmail.com>
2019-02-13 11:05:43 +0000

    Add System DTO, and API controller.
    API can currently only return an index of all systems.

---
+++ csharp/Controllers/SystemsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using csharp.Data;
+using csharp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        [HttpGet]
+        public List<SystemDTO> GetSystemsIndex()
+        {
+            var systems = _context.Systems.ToList();
+
+            List<SystemDTO> systemDTOs = new List<SystemDTO>();
+
+            foreach (var item in systems)
+            {
+                var systemModel = _context.SystemModels.Find(item.ModelId);
+                var systemType = _context.SystemTypes.Find(systemModel.TypeId);
+                var model = _context.Models.Find(systemModel.Id);
+                var manufacturer = _context.Manufacturers.Find(model.ManufacturerId);
+
+                var systemDTO = new SystemDTO()
+                {
+                    Id = item.Id,
+                    ModelName = model.Name,
+                    ManufacturerName = manufacturer.Name,
+                    SystemType = systemType.Name,
+                    HostName = item.HostName,
+                    LastUpdated = item.LastUpdated
+                };
+
+                systemDTOs.Add(systemDTO);
+            }
+
+            return systemDTOs;
+        }
+    }
+}
