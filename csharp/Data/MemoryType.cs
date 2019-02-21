5c322d66e223b47fe3c83a1c7a02b6d78127e1dd
Dan Lawrence <daniellawrence2468@gmail.com>
2019-02-21 10:59:40 +0000

    Add Model classes.

---
+++ csharp/Data/MemoryType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp.Data
+{
+    public class MemoryType
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<MemoryModel> MemoryModels { get; set; }
+    }
+}
---
+++ csharp/Data/SystemRAM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp.Data
+{
+    public class SystemRAM
+    {
+        public Guid SystemId { get; set; }
+
+        public System System { get; set; }
+
+        public Guid RAMModelId { get; set; }
+
+        public RAMModel RAMModel { get; set; }
+    }
+}
