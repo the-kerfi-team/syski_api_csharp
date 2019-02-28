d9a4aa1a448779fe4a34ddfb3249e9bd8ed5b066
JLScott1999 <git@jlscott.co.uk>
2019-02-28 14:17:03 +0000

    Add Model class.
    [Committed by Dan Lawrence]

---
+++ csharp/Data/MotherboardModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp.Data
+{
+    public class MotherboardModel
+    {
+        public Guid Id { get; set; }
+
+        public Model Model { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string Version { get; set; }
+
+        public List<System> Systems { get; set; }
+    }
+}
