c7a10ae1190641ceda5bb93b20dc30ee1252d8d5
JLScott1999 <git@jlscott.co.uk>
2019-02-28 13:39:43 +0000

    Add Models.
    [Commited by Dan Lawrence]

---
+++ csharp/Data/GPUModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp.Data
+{
+    public class GPUModel
+    {
+        public Guid Id;
+
+        public ProcessorModel ProcessorModel { get; set; }
+
+        public MemoryModel MemoryModel { get; set; }
+
+        public List<SystemGPU> SystemGPUs { get; set; }
+    }
+}
---
+++ csharp/Data/SystemGPU.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp.Data
+{
+    public class SystemGPU
+    {
+        public Guid SystemId { get; set; }
+
+        public System System { get; set; }
+
+        public Guid GPUModelId { get; set; }
+
+        public GPUModel GPUModel { get; set; }
+    }
+}
