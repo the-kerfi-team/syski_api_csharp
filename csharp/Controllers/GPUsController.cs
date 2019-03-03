ba865b1f420cc741ec32fc97bf4954a67723b56a
Dan Lawrence <daniellawrence2468@gmail.com>
2019-03-03 11:13:07 +0000

    Implement remaining APIs.

---
+++ csharp/Controllers/GPUsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using csharp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPUsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GPUsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{systemId}")]
+        public async Task<IActionResult> GetGPUs(Guid systemId)
+        {
+            if (_context.Systems.Find(systemId) == null)
+                return NotFound();
+
+            var GPUs = _context.SystemGPUs.Where(sc => sc.SystemId == systemId).ToList();
+
+            var GPUDTOs = new List<GPUDTO>();
+
+            foreach (var item in GPUs)
+            {
+                GPUDTOs.Add(CreateDTO(item));
+            }
+
+            return Ok(GPUDTOs);
+        }
+
+        private GPUDTO CreateDTO(SystemGPU systemGPU)
+        {
+            var processorModel = _context.ProcessorModels.Find(systemGPU.GPUModelId);
+            var architecture = _context.Architectures.Find(processorModel.ArchitectureId);
+            var model = _context.Models.Find(processorModel.Id);
+            var manufacturer = _context.Manufacturers.Find(model.ManufacturerId);
+            var MemoryModel = _context.MemoryModels.Find(systemGPU.GPUModelId);
+            var MemoryType = _context.MemoryTypes.Find(MemoryModel.MemoryTypeId);
+
+            var GPUDTO = new GPUDTO()
+            {
+                Id = systemGPU.GPUModelId,
+                ModelName = model.Name,
+                ManufacturerName = manufacturer.Name,
+                ArchitectureName = architecture.Name,
+                ClockSpeed = processorModel.ClockSpeed,
+                CoreCount = processorModel.CoreCount,
+                ThreadCount = processorModel.ThreadCount,
+                MemoryTypeName = MemoryType.Name,
+                MemoryBytes = MemoryModel.MemoryBytes
+            };
+
+            return GPUDTO;
+        }
+    }
+}
